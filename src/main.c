02418a4963f7803d01f9ad107ceade7c93cd47eb
skakavyponik <j.bobosik@gmail.com>
2016-10-03 11:12:10 +0200

    uloha3cela

--- src/main.c
+++ src/main.c
@@ -93,8 +93,21 @@ int main(void)
   while (1)
   {
 	  	  BUTTON = !(GPIOC->IDR &(1<<13));  //zapisovanie vstupu do premennej
-	 	  if (BUTTON==0) GPIOA->ODR |= (uint16_t)(0b1)<<5;     //zapnutie diody
-	 	  else GPIOA->ODR &= ~(uint16_t)(0b1)<<5;    // vypnutie diody
+	  	  if (BUTTON==1 && pred==1) stlacj++;					//  ak sa stlaci a chvilu nepusti ani nekmita
+	  		  else stlacj=0;
+	  		  if (stlacj>500) {
+	  			  stlacene=1;
+	  			  stlacj=500;										// aby nepretieklo
+	  		  }
+	  		  if (BUTTON==0 && pred==0) stlacn++;					// ak sa pusti a nekmita
+	  		  else stlacn=0;
+	  		  if (stlacn>500){
+	  			  stlacene=0;
+	  			  stlacn=500;  										// aby nepretieklo
+	  		  }
+	  		  if (stlacene!=predstl && stlacene==0) GPIOA->ODR ^= (uint16_t)(0b1)<<5;     //zmena stavu z 1 na 0
+	  		  predstl=stlacene;
+	  		  pred=BUTTON;
     }
   return 0;
 }
